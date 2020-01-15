ea0794227ae0a3da6a76ff774ed1285a3d5bce38
alumno <alumno@DESKTOP-TJSF3MI>
2020-01-15 14:04:36 +0100

    base de datos

--- MVVM/MainWindow.xaml.cs
+++ MVVM/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,13 @@ namespace MVVM
         public MainWindow()
         {
             InitializeComponent();
+            Tema6Entities contexto = new Tema6Entities();
+
+            contexto.CLIENTES.Load();
+
+            clientesComboBox.DataContext = contexto.CLIENTES.Local;
         }
+
+
     }
 }
